79765904006efdc7cf91eb9c1438b1e06c1521fb
BilSayfer1 <142802154+BilSayfer1@users.noreply.github.com>
2023-09-14 15:04:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let answer = prompt("Ваше имя?").toLowerCase()
+if ( answer.charAt(0) === "a") { 
+    console.log("Добро пожаловать А......." );
+
+} else  { 
+    console.log("Гуд Бай Джеки Чан");
+} 
+let chet = prompt(`Whats your name bro`)
+if (chet.length % 2 === 0)  {
+    console.log(`chetnoe imya`);
+} else {
+    console.log(`nechetnoe imya`);
+}
+let chisla  = prompt(`Enter number`) 
+if (chisla.length % 2 === 0)  {
+    console.log(`chetnoe`);
+} else {
+    console.log(`nechetnoe `);
+}
+
+
+ 
